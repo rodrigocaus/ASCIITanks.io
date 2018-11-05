2097ec980e6dd6a49b5936671264a9783ee6dac3
Victor <victorfsantolim@gmail.com>
2018-11-05 03:07:40 -0200

    Adicionada toda a parte de audio

--- src/client/mainclient.cpp
+++ src/client/mainclient.cpp
@@ -4,10 +4,13 @@
  */
 
 #include "../model.hpp"
-#include "../Som.hpp"
 #include "../Rede.hpp"
 #include "../Teclado.hpp"
 #include "../Tela.hpp"
+
+#ifdef AUDIO_ON
+	#include "../Som.hpp"
+#endif
 
 //Pega o tempo em milisegundos
 using namespace std::chrono;
@@ -51,8 +54,22 @@ int main(int argc, char *argv[])
   //Estabelece a conexao com o servidor
   cliente->conecta(nome, (size_t *) &id);
   std::cout << "Conectado com o id " << (int) id << std::endl;
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
+  #ifdef AUDIO_ON
+  //Cria objetos de audio e importa os sons que serão usados no jogo
+  Audio::Sample *somFight = new Audio::Sample();
+  somFight->load("assets/fight.dat");
+  Audio::Sample *somTiro = new Audio::Sample();
+  somTiro->load("assets/tiro.dat");
+  Audio::Sample *somBoom = new Audio::Sample();
+  somBoom->load("assets/boom.dat");
+  Audio::Sample *somHit = new Audio::Sample();
+  somHit->load("assets/hit.dat");
+  
+  //Cria o objeto de tocador de audio
+  Audio::Player *player = new Audio::Player();
+  #endif
+ 
   //Cria as listas de balas e tanques
   ListaDeBalas *ldb = new ListaDeBalas();
   ListaDeTanques *ldt = new ListaDeTanques();
@@ -74,6 +91,17 @@ int main(int argc, char *argv[])
   //Strings das listas serializadas
   std::string ldbSerial , ldtSerial;
 
+  //Variavel que indica se eh o primeiro loop, para reproduzir o som de Fight!
+  bool primeiroLoop = true;
+
+  #ifdef AUDIO_ON
+  //Varáveis dos meu estado para por comparacao com os novos recebidos, poder disparar sons
+  int minhaVidaAntes = 3;
+  int meusKillsAntes = 0;
+  int meusDeathsAntes = 0;
+  Tanque * meuTanque;
+  #endif
+
  while(1){
 
 	//Zera as strings de serial e as listas de tanques e balas, para receber as novas
@@ -93,6 +121,32 @@ int main(int argc, char *argv[])
 	ldb->deserializaLista(ldbSerial);
 	ldt->deserializaLista(ldtSerial);
 
+	#ifdef AUDIO_ON
+	if(primeiroLoop){
+		player->init();
+  		player->play(somFight);
+  		primeiroLoop = false;
+	}
+
+	//Verifica se deve tocar algum som, e se sim, qual
+	meuTanque = ldt->getTanque(id);
+	if(minhaVidaAntes > meuTanque->getVida()){
+		//Toca som de tomar hit
+        player->pause();
+        somHit->set_position(0);
+        player->play(somHit);
+
+	} else if(meusKillsAntes < meuTanque->getKills()){
+		//Toca som de explosao
+		player->pause();
+      	somBoom->set_position(0);
+      	player->play(somBoom);
+	}
+
+	minhaVidaAntes = meuTanque->getVida();
+	meusKillsAntes = meuTanque->getKills();
+	#endif
+
 	// Atualiza tela
 	tela->update();
 
@@ -100,6 +154,15 @@ int main(int argc, char *argv[])
 	char c = teclado->getChar();
     if(c != 0) {
         cliente->enviarComando(c);
+
+        #ifdef AUDIO_ON
+        //Reproduz som de tiro se tinha bala
+        if(c == ' ' && meuTanque->getBalaAtual() > 0){
+        	player->pause();
+        	somTiro->set_position(0);
+        	player->play(somTiro);
+        }
+        #endif
     }
 	if(c == 'q'){
 		break;
@@ -112,6 +175,10 @@ cliente->stop();
 tela->stop();
 teclado->stop();
 std::cout << "Fim de jogo\n";
+
+#ifdef AUDIO_ON 
+player->stop();
+#endif
 
 return 0;
 
